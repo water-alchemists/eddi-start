12102aea9140555d691a9a442ed75f4ab19982f9
Tony <terimyaki@users.noreply.github.com>
2016-03-29 21:17:30 -0400

    drafting startup script

--- initialize.sh
+++ initialize.sh
@@ -2,8 +2,29 @@
 #chkconfig: - 99 10
 
 start(){
+	SENSORS_PATH=/root/eddi-sensors/bin/sensors
+	DHCLIENT=dhclient
+	d=$(date +"%m-%d-%y %H:%M")
 	node /root/eddi-controls/initialize.js
-	echo "triggered initialize script $date" > /root/hello.txt
+	if [ test -e SENSORS_PATH ]
+	then
+		echo "eddi-sensors was already made" >> /root/hello.txt
+	else 
+		make -C /root/eddi-sensors
+	fi
+
+	if [ ps -ef | grep $SERVICE ]
+	then
+		echo "wifi was already started" >> /root/hello.txt
+	else
+		$SERVICE wlan0
+	fi
+
+	/root/eddi-sensors/bin/sensors &
+	( cd /root/eddi-persist && npm start & )
+	( cd /root/eddi-controls && npm start & )
+
+	echo "triggered initialize script $d" >> /root/hello.txt
 }
 
 case "$1" in
