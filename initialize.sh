79f85ea01acdbc559baf616a7fa1e06e44ec62d0
Tony <terimyaki@users.noreply.github.com>
2016-03-29 22:16:38 -0400

    fixed chaining syntax

--- initialize.sh
+++ initialize.sh
@@ -1,10 +1,13 @@
 #! /bin/bash
 #chkconfig: - 99 10
+LOG_PATH=/root/hello.txt
+SENSORS_PPID=''
+PERSIST_PPID=''
+CONTROLS_PPID=''
 
 start(){
 	SENSORS_PATH=/root/eddi-sensors/bin/sensors
 	DHCLIENT="dhclient"
-	LOG_PATH=/root/hello.txt
 	d=$(date +"%m-%d-%y %H:%M")
 	node /root/eddi-controls/initialize.js
 	if [ test -e SENSORS_PATH ]
@@ -25,11 +28,19 @@ start(){
 
 	fi
 
-	( /root/eddi-sensors/bin/sensors & && echo "eddi-sensors now started" >> $LOG_PATH )
-	( cd /root/eddi-persist && npm start & echo "eddi-persist now started" >> $LOG_PATH )
-	( cd /root/eddi-controls && npm start & && echo "eddi-controls now started" >> $LOG_PATH )
+	( /root/eddi-sensors/bin/sensors &; echo "eddi-sensors now started" >> $LOG_PATH; SENSORS_PPID=$PPID )
+	( cd /root/eddi-persist && npm start;echo "eddi-persist now started" >> $LOG_PATH; PERSIST_PPID=$PPID )
+	( cd /root/eddi-controls && npm start; && echo "eddi-controls now started" >> $LOG_PATH; PERSIST_PPID=$PPID )
 
 	echo "triggered initialize script $d" >> $LOG_PATH
+	exit 0
+}
+
+stop(){
+	echo "eddi-sensors process $SENSORS_PPID ended" >> $LOG_PATH
+	echo "eddi-persist process $PERSIST_PPID ended" >> $LOG_PATH
+	echo "eddi-controls process $CONTROLS_PPID ended" >> $LOG_PATH
+	exit 0
 }
 
 case "$1" in
